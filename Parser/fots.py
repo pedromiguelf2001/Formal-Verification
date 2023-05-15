351710052695359023b077f9932780b800381f1e
David Machado <machado.david190@gmail.com>
2023-05-15 16:30:01 +0100

    Added safety property generation Refactored parts of the class

--- Parser/fots.py
+++ Parser/fots.py
@@ -1,6 +1,6 @@
 from yacc import ParserPySMT
-from pprint import pprint
 from pysmt.shortcuts import *
+DEBUG = False
 
 # PC palavra reservada
 class FOTS(object):
@@ -13,30 +13,30 @@ class FOTS(object):
         self.init = self.genInit()
 
     def findStartNode(self, cfa):
-        targets = set()
-        for _, transitions in cfa.values():
-            for targetNode, _ in transitions:
-                targets.add(targetNode)
-
+        targets = {targetNode for _, transitions in cfa.values() for targetNode, _ in transitions}
         return set(cfa.keys()) - targets
 
-    # gera função que inicializa o estado na forma (state -> dict)
+    # Gera função que inicializa o estado na forma (state -> dict)
     def genInit(self):
         ini = list(self.initialNode)[0]
         code, _ = self.compiler.compile(self.cfa[ini][0])
         userIni = eval(f"lambda state: {code.replace('prox', 'state')}")
-        func =  lambda state : And(userIni(state), Equals(state["pc"], BV(0, self.state["size"])))
-        return func
+        return lambda state : And(userIni(state), Equals(state["pc"], BV(0, self.state["size"])))
+
+    def declare(self, i):
+        return {name: Symbol(f"{name}_{i}", BVType(self.state["size"])) for name in self.state["variables"]}
+
+    def safety(self, state):
+        return And([NotEquals(state["pc"], self.indices[key]) for key in self.state["error_states"]])
 
     def trans(self, curr, prox):
         debug = []
         formulas = []
         for label, (body, trans) in self.cfa.items():
             nodeBody, mutatedState = self.compiler.compile(body) if label not in self.initialNode else ("TRUE()", set())
-
-            debug.append(mutatedState)
             preservedVars = [var for var in self.state["variables"] if var not in mutatedState and var != "pc"]
             preservedFormula = And([Equals(prox[var], curr[var]) for var in preservedVars])
+            debug.append(mutatedState)
 
             for targetNode, cond in trans:
                 condFormula, _ = self.compiler.compile(cond)
@@ -49,18 +49,14 @@ class FOTS(object):
                 )
                 debug.append(formula.serialize())
                 formulas.append(formula)
-        print("DEBUG", debug)
 
+        if DEBUG: print("DEBUG", debug, "\n")
         return Or(formulas)
-    
-    def declare(self, i):
-        return {name: Symbol(f"{name}_{i}", BVType(self.state["size"])) for name in self.state["variables"]}
-
 
 
 # ---------------- Testes ----------------
-def bmc_always(fots, inv, K):
-    for k in range(1, K+1):
+def bmc_always(fots, k_max):
+    for k in range(1, k_max+1):
         trace = [fots.declare(i) for i in range(k)]
 
         # adicionar o estado inicial
@@ -68,7 +64,7 @@ def bmc_always(fots, inv, K):
         # adicionar as transições
         transitions = And([fots.trans(trace[i], trace[i+1]) for i in range(k - 1)])
         # adicionar a negação do invariante
-        invariant = Not(And([inv(trace[i], 16) for i in range(k-1)]))
+        invariant = Not(And([fots.safety(trace[i]) for i in range(k-1)]))
 
         formula = And(initialization, transitions, invariant)
         model = get_model(formula)
@@ -80,22 +76,16 @@ def bmc_always(fots, inv, K):
                 for v in trace[i]:
                     print(v, "=", model[trace[i][v]])
                 print("----------------")
-            print("O invariante não se mantém nos primeiros", k, "passos")
+            print(f"O invariante não se mantém nos primeiros {k} passos")
         else:
-            print(formula)
-            print(f"O invariante mantém-se nos primeiros {K} passos")
-
-# propriedade de segurança
-def check_inv(state, n):
-    # pc não atingir estado de erro
-    return NotEquals(state["pc"], BV(5, n))
+            print(f"O invariante mantém-se nos primeiros {k} passos")
 
 # ---------------- Main ----------------
 if __name__ == "__main__":
     # ---------------- Utilizador ----------------
     cfa = {
         "init": (
-            "x = 2; y = 4; z = 0;",
+            "x = 3; y = 4; z = 0;",
             [("switch", "")]
         ),
         "switch": (
@@ -122,9 +112,10 @@ if __name__ == "__main__":
 
     state = {
         "variables": ["pc", "x", "y", "z"],
+        "error_states": ["overflow"],
         "size": 16
     }
 
     fots = FOTS(cfa, state)
 
-    bmc_always(fots, check_inv, 15)
+    bmc_always(fots, 15)
